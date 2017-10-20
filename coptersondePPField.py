Fixed rotation matrix, added graphic to terminal, added legend for mesonet data

coptersondePPField.py
@@ -26,7 +26,7 @@ import wxtools
 ## Version ##
 #############
 '''
-Updated 27 Sep 2017
+Updated 6 Oct 2017
 Brian Greene
 University of Oklahoma
 For use with raw OU Coptersonde vertical profile data
@@ -268,19 +268,15 @@ Direction_deg[iNeg] = Direction_deg[iNeg] + 360.
 ## Automatically Determine Starting Time ##
 ###########################################
 
-updown = raw_input('>>>Enter 1 for ascent, 2 for descent, 3 for both: ')
+updown = raw_input('>>>Enter 1 for ascent or 2 for descent: ')
 if updown == '1':
     profup = 1
     profdown = 0
-    print 'Processing data for ascent'
+    print 'Selected: ascent'
 elif updown == '2':
     profup = 0
     profdown = 1
-    print 'Processing data for descent'
-elif updown == '3':
-    profup = 1
-    profdown = 1
-    print 'Processing data for ascent and descent'
+    print 'Selected: descent'
 else:
     print 'uhhhhh'
 
@@ -609,6 +605,7 @@ if mesoData.size == 0:
     umeso = np.nan
     vmeso = np.nan
     pmeso = np.nan
+    Td2meso = np.nan
 else:
     print 'Internet connection successful! Pulling Mesonet data...'
     iMesoTime = wxtools.findClosestMesoTime(timeTakeoff)
@@ -639,10 +636,12 @@ skew.plot_barbs(pres[0::4], u[0::4], v[0::4], x_clip_radius = 0.12, \
     y_clip_radius = 0.12)
 
 # Plot mesonet surface data and winds
-skew.plot(pmeso, T2meso, 'k*', linewidth=2)
-skew.plot(pres[0], T9meso, 'r*', linewidth=2)
-skew.plot(pmeso, Td2meso, 'g*', linewidth=2)
+skew.plot(pmeso, T2meso, 'k*', linewidth=2, label='Mesonet 2m T')
+skew.plot(pres[0], T9meso, 'r*', linewidth=2, label='Mesonet 9m T')
+skew.plot(pmeso, Td2meso, 'g*', linewidth=2, label='Mesonet 2m Td')
 skew.plot_barbs(pmeso, umeso, vmeso, barbcolor='r')
+
+plt.legend(loc=4)
 
 # Plot convective parameters
 if isRH:
@@ -713,7 +712,7 @@ if isRH:
     # Convective parameter values
     ax_data = fig5.add_subplot(gs[3, 2])
     plt.axis('off')
-    datastr = 'LCL = %.0f hPa\nSBCAPE = %.0f J kg$^{-1}$\n0-%.0f m bulk shear\n\
+    datastr = 'LCL = %.0f hPa\nFake News CAPE = %.0f J kg$^{-1}$\n0-%.0f m bulk shear\n\
         = %.0f kts' % \
         (lclpres.magnitude, SBCAPE.magnitude, sampleHeights_m[-3], bulkshear)
     boxprops = dict(boxstyle='round', facecolor='none')
@@ -786,3 +785,11 @@ while q != '':
 
 plt.close('all')
 print 'Post Processing Complete.'
+print '               ___________                   '
+print '              /    O      \\                 '
+print '<====>       /      U      \\       <====>   '
+print '  []________/_______________\\________[]     '
+print '           ||---------------||               '
+print '           ||               ||               '
+print '           ||               ||               '
+

wxtools.py
@@ -417,13 +417,15 @@ def parcelUAV(T, Td, p):
 	return lclpres, lcltemp, isbelowlcl, prof
 
 
-def plotUAVskewT(filenamecsv):
+def plotUAVskewT(filenamecsv, filename2=''):
 	'''
 	Input filepath of post-processed uav data
+	Optionally enter filename of 2nd csv
 
 	Outputs Skew-T log-p plot of UAV data, includes hodograph and some
 	convective parameters
 	'''
+	# File 1
 	copdata = csvread_copter(filenamecsv)
 	lat = copdata[0]
 	lon = copdata[1]
@@ -451,6 +453,23 @@ def plotUAVskewT(filenamecsv):
 	else: 
 		moist = 1
 
+	# File 2
+	if len(filename2) > 0:
+		copdata2 = csvread_copter(filename2)
+		p2 = copdata2[3]
+		T2 = copdata2[4]
+		Td2 = copdata2[5]
+		spd2 = copdata2[9]
+		spd_kts2 = spd2 * 1.94
+		dir2 = copdata2[10]
+		u2, v2 = mcalc.get_wind_components(spd_kts2*units.kts, 
+			dir2 * units.deg)
+		u2 = u2.to(units.kts)
+		v2 = v2.to(units.kts)
+	else:
+		p2, T2, Td2, Td2, spd2, spd_kts2, dir2, u2, v2 = ([] for i in range(9))
+
+
 
 	print 'Plotting...'
 	fignum = plt.figure(figsize=(12,9))
@@ -461,6 +480,12 @@ def plotUAVskewT(filenamecsv):
 	skew.plot(pressure, dewpoint, 'g', linewidth = 2)
 	skew.plot_barbs(pressure[0::4], u[0::4], v[0::4], x_clip_radius = 0.12, \
 	    y_clip_radius = 0.12)
+
+	# Plot file 2
+	if len(filename2) > 0:
+		skew.plot(p2, T2, 'r', linewidth=2, alpha=0.5)
+		skew.plot(p2, Td2, 'g', linewidth=2, alpha=0.5)
+		skew.plot_barbs(p2[0::4], u2[0::4], v2[0::4], barbcolor='r')
 
 	# Plot convective parameters
 	if moist:
@@ -474,13 +499,11 @@ def plotUAVskewT(filenamecsv):
 		isbelowlcl = 0
 
 	# set up plot limits and labels - use LCL as max if higher than profile
-	# if moist:
-	#     xmin = math.floor(np.nanmin(dewpoint)) + 2
-	# else:
-	#     xmin = math.floor(np.nanmin(temperature))
-	# xmax = math.floor(np.nanmax(temperature)) + 20
-	xmin = 0.
-	xmax = 50.
+	if moist:
+		xmin = math.floor(np.nanmin(dewpoint))
+	else:
+	    xmin = math.floor(np.nanmin(temperature))
+	xmax = math.floor(np.nanmax(temperature)) + 20
 	if isbelowlcl:
 	    ymin = round((plcl / units.mbar), -1) - 10
 	else: